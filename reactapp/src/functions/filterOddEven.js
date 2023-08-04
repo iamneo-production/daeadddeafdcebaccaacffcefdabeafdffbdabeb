98391ea42d4f2c56365a1ff12363aeb8dadce21b
Iamneo <techadmin@iamneo.ai>
2023-08-04 15:12:35 +0000

    Backup Commit

--- reactapp/src/functions/filterOddEven.js
+++ reactapp/src/functions/filterOddEven.js
@@ -1,6 +1,6 @@
 const filterOdd = (...numbers) => {
     return numbers.filter(num => num % 2 !== 0);
-  }
+}
   
 const filterEven = (...numbers) => {
     return numbers.filter(num => num % 2 === 0);
